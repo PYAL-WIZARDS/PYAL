d14b3a12fd1c42c2c04b970bd304881a13e3dbba
Baloyi Ripfumelo A <142409760+221610367@users.noreply.github.com>
2023-09-11 18:22:07 +0200

    Updated feedbackpage.php

--- feedbackpage.php
+++ feedbackpage.php
@@ -1,103 +1,43 @@
 <!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Voting Panel</title>
+<html>
+<head>
+    <title>Feedback Page</title>
+</head>
+<body>
+    <h1>Feedback Form</h1>
+    <form action="process_feedback.php" method="post">
+        <label for="name">Name:</label>
+        <input type="text" id="name" name="name" required><br><br>
 
-    <!-- Bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email" required><br><br>
 
-    <link href='http://fonts.googleapis.com/css?family=Ubuntu' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Roboto+Condensed' rel='stylesheet' type='text/css'>
+        <label for="feedback">Feedback or Issue:</label>
+        <textarea id="feedback" name="feedback" rows="4" required></textarea><br><br>
 
-    <style>
-      .headerFont{
-        font-family: 'Ubuntu', sans-serif;
-        font-size: 24px;
-      }
-
-      .subFont{
-        font-family: 'Raleway', sans-serif;
-        font-size: 14px;
-        
-      }
-      
-      .specialHead{
-        font-family: 'Oswald', sans-serif;
-      }
-
-      .normalFont{
-        font-family: 'Roboto Condensed', sans-serif;
-      }
-    </style>
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
-  <body>
-    
-  <div class="container">
-    <nav class="navbar navbar-default navbar-fixed-top navbar-inverse
-    " role="navigation">
-      <div class="container">
-        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#example-nav-collapse">
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
-        <div class="navbar-header">
-          <a href="index.html" class="navbar-brand headerFont text-lg"><strong>eVoting</strong></a>
-        </div>
-
-        <div class="collapse navbar-collapse" id="example-nav-collapse">
-          <ul class="nav navbar-nav">
-            <!--
-             <li><a href="#featuresTab"><span class="subFont"><strong>Features</strong></span></a></li>
-            <li><a href="#feedbackTab"><span class="subFont"><strong>Feedback</strong></span></a></li>
-            <li><a href="#"><span class="subFont"><strong>About</strong></span></a></li> 
-          -->
-          </ul>
-          
-
-          <button type="submit" class="btn btn-success navbar-right navbar-btn"><span class="normalFont"><strong>Admin Panel</strong></span></button>
-        </div>
-
-      </div> <!-- end of container -->
-    </nav>
-
-    <div class="container" style="padding:100px;">
-      <div class="row">
-        <div class="col-sm-12" style="border:2px solid gray;">
-          
-          <div class="page-header">
-            <h2 class="specialHead">Feedback List</h2>
-            <p class="normalFont">Feedback via your users.</p>
-          </div>
-          
-          <?php
-
-            
-
-          ?>
-
-        </div>
-      </div>
-    </div>
-  </div>
-
-
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="js/bootstrap.min.js"></script>
-  </body>
+        <input type="submit" value="Submit">
+    </form>
+</body>
 </html>
+
+
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $feedback = $_POST["feedback"];
+
+    // You can add code here to save the feedback to a database or send it via email.
+    // For demonstration purposes, we'll just display the feedback.
+
+    echo "<h1>Feedback Submitted</h1>";
+    echo "<p>Thank you, $name, for your feedback!</p>";
+    echo "<p>Email: $email</p>";
+    echo "<p>Feedback/Issue:</p>";
+    echo "<p>$feedback</p>";
+} else {
+    // Redirect to the feedback form if the request method is not POST.
+    header("Location: feedback_form.php");
+    exit();
+}
+?>
